f97d0e983f5aa0f1a76839456ba6afc5477bb59b
Daniel Goers <danielgoers@hotmail.com>
2024-01-15 16:08:18 -0500

    Add basic spring boot test

---
+++ rest-api/src/test/java/org/dsgoers/FantasyFootballApplicationTest.java
@@ -0,0 +1,22 @@
+package org.dsgoers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+public class FantasyFootballApplicationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void startsSuccessfully() {
+        assertNotNull(applicationContext);
+    }
+
+}
