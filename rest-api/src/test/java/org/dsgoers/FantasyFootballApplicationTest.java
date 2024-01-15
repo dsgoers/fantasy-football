74da4975c75e335721224e43444b321461113605
Daniel Goers <danielgoers@hotmail.com>
2024-01-15 16:14:57 -0500

    Add test configuration

--- rest-api/src/test/java/org/dsgoers/FantasyFootballApplicationTest.java
+++ rest-api/src/test/java/org/dsgoers/FantasyFootballApplicationTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 public class FantasyFootballApplicationTest {
